d1d4008647747867ecf4a21768c4da9a712a28f0
maiconcodedevelopment <codedevelopment.24@gmail.com>
2018-03-07 02:12:32 +0000

    master

---
+++ resources/views/index.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Route View System</title>
+    <style>
+        *{
+            margin:0;
+            padding: 0;
+        }
+       body{
+           height: 100vh;
+           box-sizing: border-box;
+           display:flex;
+           align-items:center;
+           justify-content:center;
+       }
+       h1{
+           text-transform: uppercase;
+           font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+           font-weight: bold;
+           letter-spacing: 1cm;
+           font-size:20px;
+           text-align:center;
+           color:black;
+       }
+    </style>
+</head>
+<body>
+  <h1>Welcome to Scripts</h1>
+</body>
+</html>
